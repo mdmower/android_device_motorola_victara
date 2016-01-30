1b3e88fe8350ebeec3337ba90aa3cf565934f5fc
Matt Mower <mowerm@gmail.com>
2016-01-30 14:33:09 -0600

    victara: Add TWRP support
    Requires updated minuitwrp
    
    Change-Id: I5882420b2c20e5257b8f26ebc13689da5ac0cdda

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -51,6 +51,7 @@ $(LZMA_RECOVERY_RAMDISK): $(recovery_ramdisk)
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 		$(LZMA_RECOVERY_RAMDISK) \
 		$(recovery_kernel)
-	$(call build-recoveryimage-target, $@)
+	$(call pretty,"Target recovery image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@ --ramdisk $(LZMA_RECOVERY_RAMDISK)
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
